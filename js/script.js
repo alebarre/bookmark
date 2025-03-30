6c86f7156530dd3dec5dfa8538baa609e2064367
alebarre <plotartemail@gmail.com>
2025-03-30 15:38:59 -0300

    Refeactoring main files to deploy

--- js/script.js
+++ js/script.js
@@ -1,30 +1,45 @@
-const tabs = document.querySelectorAll(".tab");
-const panels = document.querySelectorAll(".panel");
+const tabs = document.querySelectorAll('.tab')
+const panels = document.querySelectorAll('.panel')
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+const logo = document.getElementById('logo')
 
 // Tabs menu event listener
-tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
+tabs.forEach((tab) => tab.addEventListener('click', onTabClick))
+
+// Hamburger button listener
+btn.addEventListener('click', navToggle)
 
 function onTabClick(e) {
-  //deactivate all tabs
+  // Deactivate all tabs
   tabs.forEach((tab) => {
     tab.children[0].classList.remove(
-      "border-softRed",
-      "border-b-4",
-      "md:border-b-0"
-    );
-  });
+      'border-softRed',
+      'border-b-4',
+      'md:border-b-0'
+    )
+  })
 
-  //Hide panels
-  panels.forEach((panel) => panel.classList.add("hidden"));
+  // Hide all panels
+  panels.forEach((panel) => panel.classList.add('hidden'))
 
-  //Activate a new tab and panel based on the target
-  e.target.classList.add("border-softRed", "border-b-4");
-
-  //Removing the 'hidden' class from the 'data-target' class
-  //to show the panel
-  const classString = e.target.getAttribute("data-target");
+  // Activate a new tab and panel based on the target
+  e.target.classList.add('border-softRed', 'border-b-4')
+  const classString = e.target.getAttribute('data-target')
   document
-    .getElementById("panels")
+    .getElementById('panels')
     .getElementsByClassName(classString)[0]
-    .classList.remove("hidden");
+    .classList.remove('hidden')
+}
+
+function navToggle() {
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+
+  if (menu.classList.contains('flex')) {
+    logo.setAttribute('src', './images/logo-bookmark-footer.svg')
+  } else {
+    logo.setAttribute('src', './images/logo-bookmark.svg')
+  }
 }
